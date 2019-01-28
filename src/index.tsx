42561e51d38b852c5d900edee7dec7b9822ee25f
Tilman Frick <git@tilman.io>
2019-01-28 21:55:37 +0100

    feat: add switch component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 export * from './card';
-export * from './checkbox';
 export * from './chip';
 export * from './menu';
-export * from './radio';
+export * from './selection-control';
---
+++ src/selection-control.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import FormComponent from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import Checkbox from '@material-ui/core/Checkbox';
+import Switch from '@material-ui/core/Switch';
+import { LabelPlacement } from './types';
+
+export interface RadioProps {
+    /** @name Type @default Switch */
+    type: SelectionControlType;
+
+    /** @name Label @default Label */
+    label: string;
+
+    /** @name Checked */
+    checked?: boolean;
+
+    /** @name Disabled */
+    disabled?: boolean;
+
+    /** @name Label Position @default End */
+    labelPlacement?: LabelPlacement;
+}
+
+export enum SelectionControlType {
+    Checkbox,
+    RadioButton,
+    Switch
+}
+
+/**
+ * @name Selection Control
+ * @description Allow the user to select options
+ * @icon ToggleLeft
+ */
+export const SelectionControl: React.SFC<RadioProps> = props => {
+    return (
+        <FormComponent {...props} control={getType(props.type)} />
+    )
+}
+
+function getType(type: SelectionControlType): JSX.Element {
+	switch (type) {
+        case SelectionControlType.Checkbox:
+            return <Checkbox />;
+        case SelectionControlType.RadioButton:
+            return <Radio />;
+        case SelectionControlType.Switch:
+            return <Switch />;
+	}
+}
