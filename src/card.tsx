6cdb99715fdd264cd8b4f30da7f4b65711fb1ddc
Tilman Frick <git@tilman.io>
2019-01-28 20:31:21 +0100

    feat: add card component

---
+++ src/card.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Component from '@material-ui/core/Card';
+
+export interface CardProps {
+    /** @name Raised */
+    raised?: boolean;
+
+    /** @name Width @default 100px */
+    width: string;
+
+    /** @name Height @default 100px */
+    height: string;
+}
+
+/**
+ * @name Card
+ * @description Cards contain content and actions about a single subject
+ */
+export const Card: React.SFC<CardProps> = props => {
+    return (
+        <Component
+            raised={props.raised}
+            style={{
+                height: props.height,
+                width: props.width
+            }}
+        >
+            {props.children}
+        </Component>
+    )
+}
