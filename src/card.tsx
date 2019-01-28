76cf9ade90886b905a62cd2e6c650a90dec8656d
Tilman Frick <git@tilman.io>
2019-01-29 00:50:38 +0100

    feat: add card related components

--- src/card.tsx
+++ src/card.tsx
@@ -1,25 +1,53 @@
 import * as React from 'react';
-import Component from '@material-ui/core/Card';
+import CardComponent from '@material-ui/core/Card';
+import CardContentComponent from '@material-ui/core/CardContent';
+import CardMediaComponent from '@material-ui/core/CardMedia';
+import CardHeaderComponent from '@material-ui/core/CardHeader';
+import CardActionsComponent from '@material-ui/core/CardActions';
+
+import Avatar from '@material-ui/core/Avatar';
 
 export interface CardProps {
     /** @name Raised */
     raised?: boolean;
 
-    /** @name Width @default 100px */
+    /** @name Width @default 280px */
     width: string;
 
-    /** @name Height @default 100px */
+    /** @name Height @default 360px */
     height: string;
+}
+
+export interface CardHeaderProps {
+    /** @name Title @default Headline */
+    title: string;
+
+    /** @name Subtitle @default Subtitle */
+    subheader: string;
+}
+
+export interface CardMediaProps {
+    /** @name Image @default https://images.unsplash.com/photo-1542838775-961894306368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=360 @asset */
+    image?: string;
+
+    /** @name Height @default 120px */
+    height?: string;
+}
+
+export interface CardActionsProps {
+    /** @name No Space */
+    disableActionSpacing?: boolean;
 }
 
 /**
  * @name Card
  * @description Contain content and actions about a single subject
  * @icon Square
+ * @group Card
  */
 export const Card: React.SFC<CardProps> = props => {
     return (
-        <Component
+        <CardComponent
             raised={props.raised}
             style={{
                 height: props.height,
@@ -27,6 +55,61 @@ export const Card: React.SFC<CardProps> = props => {
             }}
         >
             {props.children}
-        </Component>
+        </CardComponent>
+    )
+}
+
+
+/**
+ * @name Card Header
+ * @icon Square
+ * @group Card
+ */
+export const CardHeader: React.SFC<CardHeaderProps> = props => {
+    return (
+        <CardHeaderComponent
+            avatar={
+                <Avatar>R</Avatar>
+            }
+            {...props}
+        />
+    )
+}
+
+/**
+ * @name Card Content
+ * @icon Square
+ */
+export const CardContent: React.SFC = props => {
+    return (
+        <CardContentComponent>
+            {props.children}
+        </CardContentComponent>
+    )
+}
+
+/**
+ * @name Card Media
+ * @icon Square
+ * @group Card
+ */
+export const CardMedia: React.SFC<CardMediaProps> = props => {
+    return (
+        <CardMediaComponent {...props} style={{
+            height: props.height
+        }} />
+    )
+}
+
+/**
+ * @name Card Actions
+ * @icon Square
+ * @group Card
+ */
+export const CardActions: React.SFC<CardActionsProps> = props => {
+    return (
+        <CardActionsComponent {...props}>
+            {props.children}
+        </CardActionsComponent>
     )
 }
