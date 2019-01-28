e329ff4b620c04420125314b01de703ff44f7691
Tilman Frick <git@tilman.io>
2019-01-28 20:44:28 +0100

    feat: add chip component

---
+++ src/chip.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Component from '@material-ui/core/Chip';
+import { Color } from './types';
+
+export interface ChipProps {
+    /** @name Label */
+    label?: string;
+
+    /** @name Delete */
+    delete?: boolean;
+
+    /** @name Color */
+    color?: Color;
+
+    /** @name Style */
+    variant?: ChipVariant;
+}
+
+export enum ChipVariant {
+    Default = 'default',
+    Outlined = 'outlined'
+}
+
+/**
+ * @name Chip
+ * @description Chips are compact elements that represent an input, attribute, or action
+ */
+export const Chip: React.SFC<ChipProps> = props => {
+    return (
+        <Component {...props} onDelete={props.delete ? (() => console.log): undefined} />
+    )
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export enum Color {
+    Primary = 'primary',
+    Secondary = 'secondary',
+    Default = 'default'
+}
