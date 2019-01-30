36bf001128d0cc58726aca1494667d0e2c0807d7
Tilman Frick <git@tilman.io>
2019-01-30 19:05:54 +0100

    feat: add expansion panel component

---
+++ src/expansion-panel.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import ExpansionPanelComponent from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelActionsComponent from '@material-ui/core/ExpansionPanelActions';
+import ExpansionPanelDetailsComponent from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummaryComponent from '@material-ui/core/ExpansionPanelSummary';
+
+export interface ExpansionPanelProps {
+    /** @name Default Expanded */
+    defaultExpanded?: boolean;
+
+    /** @name Disabled */
+    disabled?: boolean;
+
+    /** @name Expanded */
+    expanded?: boolean;
+
+    /** @name On Change */
+    onChange?: (event: React.ChangeEvent<{}>, expanded: boolean) => void;
+}
+
+export interface ExpansionPanelSummaryProps {
+    /** @name Disabled */
+    disabled?: boolean;
+
+    /** @name Expanded */
+    expanded?: boolean;
+
+    /** @name Expand Icon */
+    expandIcon?: React.ReactNode;
+
+    /** @name On Change */
+    onChange?: React.ReactEventHandler<{}>;
+}
+
+/**
+ * @name Expension Panel
+ * @description Contain creation flows and allow lightweight editing of an element
+ * @group Expension Panel
+ */
+export const ExpansionPanel: React.SFC<ExpansionPanelProps> = props => {
+    return (
+        <ExpansionPanelComponent {...props}>
+            {props.children}
+        </ExpansionPanelComponent>
+    )
+}
+
+/**
+ * @name Expension Panel Actions
+ * @group Expension Panel
+ */
+export const ExpensionPanelActions: React.SFC = props => {
+    return (
+        <ExpansionPanelActionsComponent {...props}>
+            {props.children}
+        </ExpansionPanelActionsComponent>
+    )
+}
+
+/**
+ * @name Expension Panel Details
+ * @group Expension Panel
+ */
+export const ExpensionPanelDetails: React.SFC = props => {
+    return (
+        <ExpansionPanelDetailsComponent {...props}>
+            {props.children}
+        </ExpansionPanelDetailsComponent>
+    )
+}
+
+/**
+ * @name Expension Panel Summary
+ * @group Expension Panel
+ */
+export const ExpensionPanelSummary: React.SFC<ExpansionPanelSummaryProps> = props => {
+    return (
+        <ExpansionPanelSummaryComponent {...props}>
+            {props.children}
+        </ExpansionPanelSummaryComponent>
+    )
+}
